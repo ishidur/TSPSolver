1aa3b1eea41bbf1ddfed9231ffc5674dc93ea415
Ryota Ishidu <ishidu-ryota-fz@ynu.jp>
2017-11-29 12:39:24 +0900

    files class

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
+class Files:
+    qatar = './coordinates/qatar.csv'
+    western_sahara = './coordinates/western_sahara.csv'
+    uruguay = './coordinates/uruguay.csv'
+    djibouti = './coordinates/djibouti.csv'
+    random_10_cities = './coordinates/random_10_cities.csv'
+
+
 # config
 class ENConfig:
     read_file = True
-    city_file = './coordinates/qatar.csv'
-    # city_file = './coordinates/coordinates.csv'
+    city_file = Files.djibouti
     city_num = 30
 
 
 class SOMConfig:
     read_file = False
-    city_file = 'qatar.csv'
-    # city_file = 'coordinates.csv'
+    city_file = Files.random_10_cities
     city_num = 30
