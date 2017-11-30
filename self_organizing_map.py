60e3bae1781fb76a5db716e857d1d1785ecce803
Ryota <ryota6621@gmail.com>
2017-11-30 23:55:53 +0900

    self_organizing_map.py

--- self_organizing_map.py
+++ self_organizing_map.py
@@ -1,91 +1,133 @@
-from config import SOMConfig as config
+from config import SOMConfig as Config
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 window_size = 5
-dpi = 150
+dpi = 100
 node_radius = 0.1
-iter_lim = 10000
-k_init = 0.2
-k_decay = 0.99
-alpha = 0.2
-beta = 2.1
-
-if (config.read_file):
-    np_cities = np.genfromtxt(config.city_file, delimiter=',')
-    city_num = np_cities.shape[0]
-    width_x = (np.max(np_cities[:, 0]) - np.min(np_cities[:, 0]))
-    width_y = (np.max(np_cities[:, 1]) - np.min(np_cities[:, 1]))
-    np_cities[:, 0] -= np.min(np_cities[:, 0])
-    np_cities[:, 0] /= width_x * 1.1
-    np_cities[:, 1] -= np.min(np_cities[:, 1])
-    np_cities[:, 1] /= width_y * 1.1
-    figsize = (window_size, window_size * width_y / width_x)
-else:
-    city_num = config.city_num
-    np_cities = np.random.random((city_num, 2))
-    figsize = (window_size, window_size)
-
-node_num = int(city_num * 2.5 + 0.5)
-angles = np.linspace(0, 2 * np.pi, node_num)
-np_band = np.array(
-    [node_radius * np.sin(angles) + 0.5, node_radius * np.cos(angles) + 0.5]).transpose()
-fig = plt.figure(figsize=figsize, dpi=dpi)
-plt.scatter(np_cities[:, 0], np_cities[:, 1], s=20, marker='+')
-elastic_band, = plt.plot(np_band[:, 0], np_band[:, 1])
-plt.grid()
-
-# plt.xlim(0, 1)
-# plt.ylim(0, 1)
+b_init = 10
+b_growth = 1.01
+b_ceil = 1000
+mu = 1.0
+iter_lim = 401
+record_moment = [1, 16, 31, 51, 101, 201, 401]
+record = False
 
 
-def phi(distance, k):
-    return np.exp(-distance ** 2 / (2 * k) ** 2)
+def g_func(djj_star, l, beta):
+    if djj_star < l:
+        return (1 - djj_star / l)**beta
+    return 0.0
 
 
-def dist(x, y):
-    return np.linalg.norm(x - y)
+def dist(p1, p2):
+    return np.linalg.norm(p1 - p2)
 
 
-def calc_dist_matrix(band_array, city_array):
-    dist_matrix = np.array([[dist(node, city) for node in band_array] for city in city_array])
-    return dist_matrix
+def calc_champ_node(band_array, city):
+    dist_array = np.array([dist(node, city)
+                           for node in band_array])
+    # find min value index
+    return dist_array.argmin()
 
 
-def calc_weight_matrix(band_array, k):
-    dist_matrix = calc_dist_matrix(band_array, np_cities)
-    weight_matrix = phi(dist_matrix, k)
-    for city_i in range(city_num):
-        sum = np.sum(weight_matrix[city_i, :])
-        weight_matrix[city_i, :] /= sum
-    return weight_matrix
-
-
-def update_node(band_array, index, weights):
-    back_i = (index - 1) % node_num
-    forward_i = (index + 1) % node_num
-    attraction_force = np.zeros(2)
-    for city_i in range(city_num):
-        attraction_force += weights[city_i, index] * (np_cities[city_i, :] - band_array[index, :])
-    delta_node = alpha * attraction_force + beta * k * (
-        band_array[forward_i, :] - 2 * band_array[index, :] + band_array[back_i, :])
+def update_node(node, city, djj_star, beta):
+    delta_node = mu * g_func(djj_star, node_num / 2, beta) * (city - node)
     return delta_node
 
 
-def update_band(band_array, weights):
+def update_band(band_array, city, j_star, beta):
     new_band_array = band_array.copy()
-    for i in range(node_num):
-        new_band_array[i, :] += update_node(band_array, i, weights)
+    for j in range(node_num):
+        djj_star = np.amin([np.abs(j - j_star), node_num - np.abs(j - j_star)])
+        new_band_array[
+            j, :] += update_node(band_array[j, :], city, djj_star, beta)
     return new_band_array
 
 
+def make_directory():
+    dir_name = './results/'
+    directory = os.path.dirname(dir_name)
+    try:
+        os.stat(directory)
+    except:
+        os.mkdir(directory)
+    dir_name += Config.city_file.replace(
+        '.csv', '') + '/'
+    directory = os.path.dirname(dir_name)
+    try:
+        os.stat(directory)
+    except:
+        os.mkdir(directory)
+    dir_name += 'self_organizing_map/'
+    directory = os.path.dirname(dir_name)
+    try:
+        os.stat(directory)
+    except:
+        os.mkdir(directory)
+    return dir_name
+
+
+def som_begin(band_array, city_array):
+    beta = b_init
+    np.random.shuffle(city_array)
+    if record:
+        dir_name = make_directory()
+        for i in range(iter_lim):
+            picked_city = city_array[i % city_num, :]
+            beta = np.amin([b_ceil, beta * b_growth])
+            j_star = calc_champ_node(band_array, picked_city)
+            band_array = update_band(band_array, picked_city, j_star, beta)
+            circle_band = np.vstack((band_array, band_array[0, :]))
+            plt.title("iteration=" + str(i))
+            elastic_band.set_data(circle_band[:, 0], circle_band[:, 1])
+            plt.pause(.001)
+            if i + 1 in record_moment:
+                filename = 'iteration-' + str(i) + '.png'
+                file_path = dir_name + filename
+                plt.savefig(file_path)
+    else:
+        i = 1
+        while plt.get_fignums():
+            picked_city = city_array[i % city_num, :]
+            beta = np.amin([b_ceil, beta * b_growth])
+            j_star = calc_champ_node(band_array, picked_city)
+            band_array = update_band(band_array, picked_city, j_star, beta)
+            circle_band = np.vstack((band_array, band_array[0, :]))
+            plt.title("iteration=" + str(i))
+            elastic_band.set_data(circle_band[:, 0], circle_band[:, 1])
+            i += 1
+            plt.pause(.001)
+
+
 if __name__ == "__main__":
-    k = k_init
-    # for i in range(iter_lim):
-    while True:
-        k = np.amax([0.01, k * k_decay])
-        weights = calc_weight_matrix(np_band, k)
-        np_band = update_band(np_band, weights)
-        circle_band = np.vstack((np_band, np_band[0, :]))
-        elastic_band.set_data(circle_band[:, 0], circle_band[:, 1])
-        plt.pause(.01)
+    if (Config.read_file):
+        np_cities = np.genfromtxt(
+            Config.file_path + Config.city_file, delimiter=',')
+        city_num = np_cities.shape[0]
+        width_x = (np.max(np_cities[:, 0]) - np.min(np_cities[:, 0]))
+        width_y = (np.max(np_cities[:, 1]) - np.min(np_cities[:, 1]))
+        width = np.amax([width_x, width_y])
+        np_cities[:, 0] -= np.min(np_cities[:, 0])
+        np_cities[:, 0] /= width
+        np_cities[:, 1] -= np.min(np_cities[:, 1])
+        np_cities[:, 1] /= width
+        figsize = (window_size, window_size)
+    else:
+        city_num = Config.city_num
+        # “continuous uniform” distribution random
+        np_cities = np.random.random((city_num, 2))
+        figsize = (window_size, window_size)
+
+    node_num = int(city_num * 2)
+    angles = np.linspace(0, 2 * np.pi, node_num)
+    np_band = np.array(
+        [node_radius * np.sin(angles) + 0.5, node_radius * np.cos(angles) + 0.5]).transpose()
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    plt.scatter(np_cities[:, 0], np_cities[:, 1], s=20, marker='+')
+    elastic_band, = plt.plot(np_band[:, 0], np_band[:, 1])
+    plt.title("iteration=" + str(0))
+    plt.grid()
+    plt.pause(.001)
+    som_begin(np_band, np_cities)
